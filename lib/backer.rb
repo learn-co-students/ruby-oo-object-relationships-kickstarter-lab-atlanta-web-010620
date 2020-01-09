20cd7a7a6c82c958e656d804a1333e0671e3e6e6
Rashad Dixon <dixoncodes@gmail.com>
2020-01-09 13:25:56 -0500

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Backer
+    attr_reader :name
+
+    
+    def initialize(name)
+        @name = name
+        @projects = []
+    end
+    
+    def back_project(project)
+        ProjectBacker.new(project, self)
+        @projects << project
+    end
+
+    def backed_projects
+        @projects
+    end
+end
